3b388046086b44dc48b6157f8d6d83b84168c36d
mdarlow <mdarlow@gmail.com>
2020-07-21 18:11:08 -0700

    Add project files.

---
+++ VisualStudio.website/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function move() {
+    var element = document.getElementById("animate");
+    var position = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (position == 350) {
+            clearInterval(id);
+        } else {
+            position++;
+            element.style.top = position + "px";
+            element.style.left = position + "px";
+        }
+    }
+}
